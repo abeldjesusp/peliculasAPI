83ef79e12cdf68270f7c79f21028ed5f16ccfb70
Abel De Jesus <adejesus@contractor.appsbees.com>
2021-08-16 10:02:41 -0400

    Initial commit

---
+++ PeliculasApi/Entidades/Genero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using PeliculasApi.Validaciones;
+
+namespace PeliculasApi.Entidades
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [StringLength(maximumLength:50)]
+        [PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+
+        public Genero()
+        {
+                
+        }
+    }
+}
