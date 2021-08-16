de78af074fe85c048153a852bd1a836e057ff6e4
Abel De Jesus <adejesus@contractor.appsbees.com>
2021-08-16 09:59:33 -0400

    Initial commit

---
+++ PeliculasApi/Controllers/GenerosController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PeliculasApi.Entidades;
+
+namespace PeliculasApi.Controllers
+{
+    [Route("api/generos")]
+    [ApiController]
+    public class GenerosController: ControllerBase
+    {
+        private readonly ILogger<GenerosController> logger;
+
+        public GenerosController(ILogger<GenerosController> logger)
+        {
+           
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Genero>> Get()
+        {
+            return new List<Genero>() { new Genero() { Id = 1, Nombre = "Comedia" } };
+        }
+
+        [HttpGet("{Id:int}")]
+        public async Task<ActionResult<Genero>> Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Genero genero)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public ActionResult Put()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
